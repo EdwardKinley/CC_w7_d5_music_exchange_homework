40c692548de3551f1f3e1361931ac5ca037eb43b
EdwardKinley <edward_kinley@yahoo.co.uk>
2018-07-06 17:50:11 +0100

    adds Shop class

--- src/main/java/Shop.java
+++ src/main/java/Shop.java
@@ -1,13 +1,22 @@
-//public class Shop {
-//
-//    private String name;
-//    private ArrayList<ISell> stock;
-//
-//    public Shop (String name, ArrayList<ISell> stock) {
-//        this.name = name;
-//        this.stock = stock;
-//    }
-//
-//
-//
-//}
+import interfaces.ISell;
+
+import java.util.ArrayList;
+
+public class Shop {
+
+    private String name;
+    private ArrayList<ISell> stock;
+
+    public Shop (String name, ArrayList<ISell> stock) {
+        this.name = name;
+        this.stock = stock;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ArrayList<ISell> getStock() {
+        return stock;
+    }
+}
--- src/test/java/ShopTest.java
+++ src/test/java/ShopTest.java
@@ -1,8 +1,31 @@
-//public class ShopTest {
-//
-//    Shop shop;
-//
-//
-//
-//
-//}
+import interfaces.ISell;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+
+public class ShopTest {
+
+    Shop shop1;
+    ArrayList<ISell> stock;
+
+    @Before
+    public void before() {
+        stock = new ArrayList<>();
+        shop1 = new Shop("Ray's Music Exchange II", stock);
+    }
+
+    @Test
+    public void canGetName() {
+        assertEquals("Ray's Music Exchange II", shop1.getName());
+    }
+
+    @Test
+    public void beginsWithNoStock() {
+        assertEquals(0, shop1.getStock().size());
+    }
+
+
+}
