b38624d9ae657308a50bc1430c1b97f05506cb3f
EdwardKinley <edward_kinley@yahoo.co.uk>
2018-07-06 16:52:54 +0100

    adds and tests trumpet class

---
+++ src/main/java/instruments/Trumpet.java
@@ -0,0 +1,32 @@
+package instruments;
+
+import instruments.instrumentproperties.ColourType;
+import instruments.instrumentproperties.FamilyType;
+import instruments.instrumentproperties.MaterialType;
+import interfaces.IPlay;
+
+public class Trumpet extends Instrument implements IPlay {
+
+    private int numberOfValves;
+
+    public Trumpet(
+            int buyPrice,
+            int sellPrice,
+            MaterialType material,
+            ColourType colour,
+            FamilyType family,
+            int numberOfValves) {
+        super(buyPrice, sellPrice, material, colour, family);
+        this.numberOfValves = numberOfValves;
+    }
+
+    public int getNumberOfValves() {
+        return numberOfValves;
+    }
+
+    public String play() {
+        return "parp";
+    }
+
+
+}
---
+++ src/test/java/instruments/TrumpetTest.java
@@ -0,0 +1,61 @@
+package instruments;
+
+import instruments.instrumentproperties.ColourType;
+import instruments.instrumentproperties.FamilyType;
+import instruments.instrumentproperties.MaterialType;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TrumpetTest {
+
+    Trumpet trumpet;
+
+    @Before
+    public void before() {
+        trumpet = new Trumpet(100, 140, MaterialType.WOOD, ColourType.BROWN, FamilyType.STRING, 3);
+    }
+
+    @Test
+    public void canGetBuyPrice() {
+        assertEquals(100, trumpet.getBuyPrice());
+    }
+
+    @Test
+    public void canGetSellPrice() {
+        assertEquals(140, trumpet.getSellPrice());
+    }
+
+    @Test
+    public void canGetNumberOfStrings() {
+        assertEquals(3, trumpet.getNumberOfValves());
+    }
+
+    @Test
+    public void canGetMarkup() {
+        assertEquals(40, trumpet.calculateMarkup());
+    }
+
+    @Test
+    public void canGetMaterial() {
+        assertEquals(MaterialType.WOOD, trumpet.getMaterial());
+    }
+
+    @Test
+    public void canGetColour() {
+        assertEquals(ColourType.BROWN, trumpet.getColour());
+    }
+
+    @Test
+    public void canGetFamily() {
+        assertEquals(FamilyType.STRING, trumpet.getFamily());
+    }
+
+    @Test
+    public void canPlay() {
+        assertEquals("parp", trumpet.play());
+    }
+
+
+}
